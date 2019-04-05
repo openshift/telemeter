7471bfac9ecfffe33faff7b2d2aac60474099b7d
Clayton Coleman <ccoleman@redhat.com>
2019-04-05 17:10:27 -0400

    debug: Add the block mutex handler
    This is inert unless someone sets runtime.SetBlockProfileRate

--- pkg/http/routes.go
+++ pkg/http/routes.go
@@ -13,6 +13,7 @@ func DebugRoutes(mux *http.ServeMux) *http.ServeMux {
 	mux.HandleFunc("/debug/pprof/", pprof.Index)
 	mux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
 	mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
+	mux.Handle("/debug/pprof/block", pprof.Handler("block"))
 	mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
 	mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
 	return mux
