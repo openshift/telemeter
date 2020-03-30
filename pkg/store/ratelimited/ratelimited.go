cdcc68d80b201f7a6183e8734b26c99a3a733711
Matthias Loibl <mail@matthiasloibl.com>
2020-03-30 17:49:28 +0200

    pkg/store/ratelimited: Create HTTP middleware

--- pkg/store/ratelimited/ratelimited.go
+++ pkg/store/ratelimited/ratelimited.go
@@ -3,11 +3,14 @@ package ratelimited
 import (
 	"context"
 	"fmt"
+	"net/http"
 	"sync"
 	"time"
 
-	"github.com/openshift/telemeter/pkg/store"
 	"golang.org/x/time/rate"
+
+	"github.com/openshift/telemeter/pkg/store"
+	"github.com/openshift/telemeter/pkg/validate"
 )
 
 type ErrWriteLimitReached string
@@ -65,4 +68,48 @@ func (s *lstore) limiter(partitionKey string) *rate.Limiter {
 	}
 
 	return limiter
+}
+
+func Middleware(limit time.Duration, now func() time.Time, next http.HandlerFunc) http.HandlerFunc {
+	s := middlewareStore{
+		limits: make(map[string]*rate.Limiter),
+		mu:     sync.Mutex{},
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		partitionKey, ok := validate.PartitionFromContext(r.Context())
+		if !ok {
+			http.Error(w, "failed to get partition from request", http.StatusInternalServerError)
+			return
+		}
+
+		if err := s.Limit(limit, now(), partitionKey); err != nil {
+			http.Error(w, err.Error(), http.StatusTooManyRequests)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	}
+}
+
+type middlewareStore struct {
+	limits map[string]*rate.Limiter
+	mu     sync.Mutex
+}
+
+func (s *middlewareStore) Limit(limit time.Duration, now time.Time, key string) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	limiter, ok := s.limits[key]
+	if !ok {
+		limiter = rate.NewLimiter(rate.Every(limit), 1)
+		s.limits[key] = limiter
+	}
+
+	if !limiter.AllowN(now, 1) {
+		return ErrWriteLimitReached(key)
+	}
+
+	return nil
 }
--- pkg/store/ratelimited/ratelimited_test.go
+++ pkg/store/ratelimited/ratelimited_test.go
@@ -2,10 +2,19 @@ package ratelimited
 
 import (
 	"context"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"sync"
 	"testing"
 	"time"
 
+	"golang.org/x/time/rate"
+
+	"github.com/openshift/telemeter/pkg/authorize"
 	"github.com/openshift/telemeter/pkg/store"
+	"github.com/openshift/telemeter/pkg/validate"
 )
 
 type testStore struct{}
@@ -85,6 +94,160 @@ func TestWriteMetrics(t *testing.T) {
 
 			if got := s.writeMetrics(ctx, tc.metrics, now); got != tc.expectedErr {
 				t.Errorf("expected err %v, got %v", tc.expectedErr, got)
+			}
+		})
+	}
+}
+
+func TestMiddleware(t *testing.T) {
+	clientID := func() string { return "" }
+
+	fakeAuthorizeHandler := func(next http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			r = r.WithContext(authorize.WithClient(r.Context(), &authorize.Client{
+				Labels: map[string]string{"_id": clientID()},
+			}))
+			next.ServeHTTP(w, r)
+		}
+	}
+	server := httptest.NewServer(
+		fakeAuthorizeHandler(
+			validate.PartitionKey("_id",
+				Middleware(time.Minute, time.Now,
+					func(w http.ResponseWriter, r *http.Request) {},
+				),
+			),
+		),
+	)
+
+	defer server.Close()
+
+	for _, tc := range []struct {
+		name           string
+		advance        time.Duration
+		clientID       string
+		expectedStatus int
+		expectedErr    error
+	}{
+		{
+			name:           "WriteSuccessForA",
+			advance:        0,
+			clientID:       "a",
+			expectedStatus: http.StatusOK,
+			expectedErr:    nil,
+		},
+		{
+			name:           "WriteSuccessForB",
+			advance:        0,
+			clientID:       "b",
+			expectedStatus: http.StatusOK,
+			expectedErr:    nil,
+		},
+		{
+			name:           "FailAfter1sForA",
+			advance:        time.Second,
+			clientID:       "a",
+			expectedStatus: http.StatusTooManyRequests,
+			expectedErr:    ErrWriteLimitReached("a"),
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			clientID = func() string { return tc.clientID }
+
+			time.Sleep(tc.advance)
+
+			req, err := http.NewRequest(http.MethodPost, server.URL, nil)
+			if err != nil {
+				t.Error("failed to create request")
+				return
+			}
+
+			resp, err := http.DefaultClient.Do(req)
+			if err != nil {
+				t.Errorf("failed to do request: %v", err)
+				return
+			}
+
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer resp.Body.Close()
+
+			if resp.StatusCode != tc.expectedStatus {
+				t.Errorf("expected status %d and got %d: %s", tc.expectedStatus, resp.StatusCode, string(body))
+			}
+
+			if tc.expectedErr != nil {
+				if strings.TrimSpace(string(body)) != tc.expectedErr.Error() {
+					t.Errorf("expcted body '%s', and got '%s'", tc.expectedErr.Error(), strings.TrimSpace(string(body)))
+				}
+			}
+		})
+	}
+}
+
+func Test_middlewareStore_Limit(t *testing.T) {
+	s := &middlewareStore{
+		limits: map[string]*rate.Limiter{},
+		mu:     sync.Mutex{},
+	}
+
+	now := time.Time{}.Add(time.Hour)
+
+	for _, tc := range []struct {
+		name    string
+		advance time.Duration
+		key     string
+		err     error
+	}{
+		{
+			name:    "first",
+			advance: 0,
+			key:     "a",
+			err:     nil,
+		},
+		{
+			name:    "1sfails",
+			advance: time.Second,
+			key:     "a",
+			err:     ErrWriteLimitReached("a"),
+		},
+		{
+			name:    "10sfails",
+			advance: 10 * time.Second,
+			key:     "a",
+			err:     ErrWriteLimitReached("a"),
+		},
+		{
+			name:    "10sSuccessForB",
+			advance: 10 * time.Second,
+			key:     "b",
+			err:     nil,
+		},
+		{
+			name:    "1minSuccess",
+			advance: time.Minute,
+			key:     "a",
+			err:     nil,
+		},
+		{
+			name:    "2minSuccess",
+			advance: 2 * time.Minute,
+			key:     "a",
+			err:     nil,
+		},
+		{
+			name:    "2minSuccessForB",
+			advance: 2 * time.Minute,
+			key:     "b",
+			err:     nil,
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			err := s.Limit(time.Minute, now.Add(tc.advance), tc.key)
+			if err != tc.err {
+				t.Errorf("expected '%s' got '%s'", tc.err, err)
 			}
 		})
 	}
