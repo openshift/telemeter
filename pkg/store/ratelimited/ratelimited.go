1d08da2f64b3a2b6e27f3a88215f3da1b7e974e4
Matthias Loibl <mail@matthiasloibl.com>
2020-03-31 15:41:06 +0200

    Delete unused store.Store interface

--- pkg/store/ratelimited/ratelimited.go
+++ pkg/store/ratelimited/ratelimited.go
@@ -1,7 +1,6 @@
 package ratelimited
 
 import (
-	"context"
 	"fmt"
 	"net/http"
 	"sync"
@@ -9,7 +8,6 @@ import (
 
 	"golang.org/x/time/rate"
 
-	"github.com/openshift/telemeter/pkg/store"
 	"github.com/openshift/telemeter/pkg/validate"
 )
 
@@ -17,57 +15,6 @@ type ErrWriteLimitReached string
 
 func (e ErrWriteLimitReached) Error() string {
 	return fmt.Sprintf("write limit reached for key %q", string(e))
-}
-
-type lstore struct {
-	limit time.Duration
-	next  store.Store
-
-	mu    sync.RWMutex // protects fields below
-	store map[string]*rate.Limiter
-}
-
-// New returns a store that wraps next and limits writes to it.
-// Writes can happen at most at intervals specified by limit per partition key.
-func New(limit time.Duration, next store.Store) *lstore {
-	return &lstore{
-		limit: limit,
-		next:  next,
-		store: make(map[string]*rate.Limiter),
-	}
-}
-
-func (s *lstore) ReadMetrics(ctx context.Context, minTimestampMs int64) ([]*store.PartitionedMetrics, error) {
-	return s.next.ReadMetrics(ctx, minTimestampMs)
-}
-
-func (s *lstore) WriteMetrics(ctx context.Context, p *store.PartitionedMetrics) error {
-	return s.writeMetrics(ctx, p, time.Now())
-}
-
-func (s *lstore) writeMetrics(ctx context.Context, p *store.PartitionedMetrics, now time.Time) error {
-	if p == nil {
-		return nil
-	}
-
-	if limiter := s.limiter(p.PartitionKey); !limiter.AllowN(now, 1) {
-		return ErrWriteLimitReached(p.PartitionKey)
-	}
-
-	return s.next.WriteMetrics(ctx, p)
-}
-
-func (s *lstore) limiter(partitionKey string) *rate.Limiter {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-
-	limiter, ok := s.store[partitionKey]
-	if !ok {
-		limiter = rate.NewLimiter(rate.Every(s.limit), 1)
-		s.store[partitionKey] = limiter
-	}
-
-	return limiter
 }
 
 func Middleware(limit time.Duration, now func() time.Time, next http.HandlerFunc) http.HandlerFunc {
--- pkg/store/store.go
+++ pkg/store/store.go
@@ -1,17 +1,10 @@
 package store
 
 import (
-	"context"
-
 	clientmodel "github.com/prometheus/client_model/go"
 )
 
 type PartitionedMetrics struct {
 	PartitionKey string
 	Families     []*clientmodel.MetricFamily
-}
-
-type Store interface {
-	ReadMetrics(ctx context.Context, minTimestampMs int64) ([]*PartitionedMetrics, error)
-	WriteMetrics(context.Context, *PartitionedMetrics) error
 }
