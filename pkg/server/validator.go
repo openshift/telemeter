0f78d483f165af2f233dfb751be20359d344540f
Matthias Loibl <mail@matthiasloibl.com>
2020-04-03 16:05:37 +0200

    Refactor Validator to HTTP middleware

--- pkg/server/validator.go
+++ pkg/server/validator.go
@@ -1,63 +1,24 @@
 package server
 
 import (
+	"bytes"
 	"context"
+	"errors"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"time"
 
+	clientmodel "github.com/prometheus/client_model/go"
+	"github.com/prometheus/common/expfmt"
+
 	"github.com/openshift/telemeter/pkg/authorize"
 	"github.com/openshift/telemeter/pkg/metricfamily"
+	"github.com/openshift/telemeter/pkg/reader"
 )
 
-//
-//// Validator validates an upload.
-//type Validator interface {
-//	Validate(ctx context.Context, req *http.Request) (string, metricfamily.Transformer, error)
-//}
-//
-//type validator struct {
-//	partitionKey string
-//	limitBytes   int64
-//	maxAge       time.Duration
-//	nowFunc      func() time.Time
-//}
-//
-//// New handles Prometheus metrics from end clients that must be assumed to be hostile.
-//// It implements metrics transforms that sanitize the incoming content.
-//func New(partitionKey string, limitBytes int64, maxAge time.Duration, nowFunc func() time.Time) Validator {
-//	return &validator{
-//		limitBytes: limitBytes,
-//		maxAge:     maxAge,
-//		nowFunc:    nowFunc,
-//	}
-//}
-
-//// Validate implements the Validator interface. It validates an upload.
-//func (v *validator) Validate(ctx context.Context, req *http.Request) (string, metricfamily.Transformer, error) {
-//	client, ok := authorize.FromContext(ctx)
-//	if !ok {
-//		return "", nil, fmt.Errorf("unable to find user info")
-//	}
-//
-//	var transforms metricfamily.MultiTransformer
-//
-//	if v.maxAge > 0 {
-//		transforms.With(metricfamily.NewErrorInvalidFederateSamples(time.Now().Add(-v.maxAge)))
-//	}
-//
-//	transforms.With(metricfamily.NewErrorOnUnsorted(true))
-//	transforms.With(metricfamily.NewRequiredLabels(client.Labels))
-//	transforms.With(metricfamily.TransformerFunc(metricfamily.DropEmptyFamilies))
-//	transforms.With(metricfamily.OverwriteTimestamps(v.nowFunc))
-//
-//	if v.limitBytes > 0 {
-//		req.Body = reader.NewLimitReadCloser(req.Body, v.limitBytes)
-//	}
-//
-//	return client.Labels[v.partitionKey], transforms, nil
-//}
-
+// PartitionKey is a HTTP middleware that extracts the partitionKey and passes it on via context.
 func PartitionKey(key string, next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		client, ok := authorize.FromContext(r.Context())
@@ -76,10 +37,12 @@ func PartitionKey(key string, next http.HandlerFunc) http.HandlerFunc {
 	}
 }
 
+// WithPartition puts the partitionKey into the given context.
 func WithPartition(ctx context.Context, partition string) context.Context {
 	return context.WithValue(ctx, partitionCtx, partition)
 }
 
+// PartitionFromContext returns the partitionKey from the context.
 func PartitionFromContext(ctx context.Context) (string, bool) {
 	p, ok := ctx.Value(partitionCtx).(string)
 	return p, ok
@@ -91,14 +54,78 @@ const (
 	partitionCtx partitionCtxType = iota
 )
 
-func ByteLimit(limit int64, next http.HandlerFunc) http.HandlerFunc {
+// Validate the payload of a request against given and required rules.
+func Validate(maxAge time.Duration, limitBytes int64, now func() time.Time, next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-	}
-}
+		client, ok := authorize.FromContext(r.Context())
+		if !ok {
+			http.Error(w, "unable to find user info", http.StatusInternalServerError)
+			return
+		}
 
-func MaxAge(maxAge time.Duration, next http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		metricfamily.NewErrorInvalidFederateSamples(time.Now().Add(-maxAge))
+		var transforms metricfamily.MultiTransformer
+		transforms.With(metricfamily.NewErrorOnUnsorted(true))
+		transforms.With(metricfamily.NewRequiredLabels(client.Labels))
+		transforms.With(metricfamily.TransformerFunc(metricfamily.DropEmptyFamilies))
+
+		if limitBytes > 0 {
+			r.Body = reader.NewLimitReadCloser(r.Body, limitBytes)
+		}
+
+		// these transformers need to be created for every request
+		if maxAge > 0 {
+			transforms.With(metricfamily.NewErrorInvalidFederateSamples(time.Now().Add(-maxAge)))
+		}
+
+		transforms.With(metricfamily.OverwriteTimestamps(now))
+
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer r.Body.Close()
+
+		r.Body = ioutil.NopCloser(bytes.NewBuffer(body))
+
+		decoder := expfmt.NewDecoder(r.Body, expfmt.ResponseFormat(r.Header))
+
+		families := make([]*clientmodel.MetricFamily, 0, 100)
+		for {
+			family := &clientmodel.MetricFamily{}
+			if err := decoder.Decode(family); err != nil {
+				if err == io.EOF {
+					break
+				}
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			families = append(families, family)
+		}
+		families = metricfamily.Pack(families)
+
+		if err := metricfamily.Filter(families, transforms); err != nil {
+			if errors.Is(err, metricfamily.ErrNoTimestamp) {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+			if errors.Is(err, metricfamily.ErrUnsorted) {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+			if errors.Is(err, metricfamily.ErrTimestampTooOld) {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+			if errors.Is(err, metricfamily.ErrRequiredLabelMissing) {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
 		next.ServeHTTP(w, r)
 	}
 }
---
+++ pkg/server/validator_test.go
@@ -0,0 +1,152 @@
+package server
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+	"time"
+
+	clientmodel "github.com/prometheus/client_model/go"
+	"github.com/prometheus/common/expfmt"
+
+	"github.com/openshift/telemeter/pkg/authorize"
+)
+
+func TestValidate(t *testing.T) {
+	fooMetricName := "foo_metric"
+	fooLabelName := "foo"
+	fooLabelValue1 := "bar"
+	clientIDLabelName := "_id"
+	clientIDLabelValue := "test"
+	value42 := 42.0
+	timestamp := int64(15615582020000)
+	timestampNewer := int64(15615582020000 + 10000)
+	timestampTooOld := int64(1234)
+
+	fakeAuthorizeHandler := func(next http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			r = r.WithContext(authorize.WithClient(r.Context(), &authorize.Client{
+				Labels: map[string]string{"_id": "test"},
+			}))
+			next.ServeHTTP(w, r)
+		}
+	}
+
+	s := httptest.NewServer(
+		fakeAuthorizeHandler(
+			Validate(time.Hour, 512*1024, time.Now,
+				func(w http.ResponseWriter, r *http.Request) {},
+			),
+		),
+	)
+	defer s.Close()
+
+	testcases := []struct {
+		name         string
+		families     []*clientmodel.MetricFamily
+		expectStatus int
+		expectBody   string
+	}{{
+		name: "noTimestamp",
+		families: []*clientmodel.MetricFamily{{
+			Name: &fooMetricName,
+			Metric: []*clientmodel.Metric{{
+				Label: []*clientmodel.LabelPair{
+					{Name: &clientIDLabelName, Value: &clientIDLabelValue},
+					{Name: &fooLabelName, Value: &fooLabelValue1},
+				},
+				Counter: &clientmodel.Counter{Value: &value42},
+			}},
+		}},
+		expectStatus: http.StatusBadRequest,
+		expectBody:   "metrics in provided family do not have a timestamp",
+	}, {
+		name: "unsorted",
+		families: []*clientmodel.MetricFamily{{
+			Name: &fooMetricName,
+			Metric: []*clientmodel.Metric{{
+				Label: []*clientmodel.LabelPair{
+					{Name: &clientIDLabelName, Value: &clientIDLabelValue},
+					{Name: &fooLabelName, Value: &fooLabelValue1},
+				},
+				Counter:     &clientmodel.Counter{Value: &value42},
+				TimestampMs: &timestampNewer,
+			}, {
+				Label: []*clientmodel.LabelPair{
+					{Name: &clientIDLabelName, Value: &clientIDLabelValue},
+					{Name: &fooLabelName, Value: &fooLabelValue1},
+				},
+				Counter:     &clientmodel.Counter{Value: &value42},
+				TimestampMs: &timestamp,
+			}},
+		}},
+		expectStatus: http.StatusBadRequest,
+		expectBody:   "metrics in provided family are not in increasing timestamp order",
+	}, {
+		name: "tooOld",
+		families: []*clientmodel.MetricFamily{{
+			Name: &fooMetricName,
+			Metric: []*clientmodel.Metric{{
+				Label: []*clientmodel.LabelPair{
+					{Name: &clientIDLabelName, Value: &clientIDLabelValue},
+					{Name: &fooLabelName, Value: &fooLabelValue1},
+				},
+				Counter:     &clientmodel.Counter{Value: &value42},
+				TimestampMs: &timestampTooOld,
+			}},
+		}},
+		expectStatus: http.StatusBadRequest,
+		expectBody:   "metrics in provided family have a timestamp that is too old, check clock skew",
+	}, {
+		name: "missingRequiredLabel",
+		families: []*clientmodel.MetricFamily{{
+			Name: &fooMetricName,
+			Metric: []*clientmodel.Metric{{
+				Label: []*clientmodel.LabelPair{
+					{Name: &fooLabelName, Value: &fooLabelValue1},
+				},
+				Counter:     &clientmodel.Counter{Value: &value42},
+				TimestampMs: &timestampTooOld,
+			}},
+		}},
+		expectStatus: http.StatusBadRequest,
+		expectBody:   "a required label is missing from the metric",
+	}}
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			buf := &bytes.Buffer{}
+			encoder := expfmt.NewEncoder(buf, expfmt.FmtText)
+			for _, f := range tc.families {
+				if err := encoder.Encode(f); err != nil {
+					t.Fatal(err)
+				}
+			}
+
+			req, err := http.NewRequest(http.MethodPost, s.URL, buf)
+			if err != nil {
+				t.Fatal(err)
+			}
+			req.Header.Set("Content-Type", string(expfmt.FmtText))
+
+			resp, err := s.Client().Do(req)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if resp.StatusCode != tc.expectStatus {
+				t.Errorf("expected status code %d but got %d: %s", tc.expectStatus, resp.StatusCode, string(body))
+			}
+			if strings.TrimSpace(string(body)) != tc.expectBody {
+				t.Errorf("expected body to be '%s' but got '%s'", tc.expectBody, strings.TrimSpace(string(body)))
+			}
+		})
+	}
+}
