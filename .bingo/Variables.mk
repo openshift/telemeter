416b785959f3d8cc7b721a4e92bce1ddad02058d
Kemal Akkoyun <kakkoyun@gmail.com>
2020-06-16 16:08:07 +0200

    Add mod=readonly flag
    Signed-off-by: Kemal Akkoyun <kakkoyun@gmail.com>

--- .bingo/Variables.mk
+++ .bingo/Variables.mk
@@ -20,40 +20,40 @@ EMBEDMD := $(GOBIN)/embedmd-v1.0.0
 $(EMBEDMD): .bingo/embedmd.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
 	@echo "(re)installing $(GOBIN)/embedmd-v1.0.0"
-	@cd .bingo && $(GO) build -modfile=embedmd.mod -o=$(GOBIN)/embedmd-v1.0.0 "github.com/campoy/embedmd"
+	@cd .bingo && $(GO) build -modfile=embedmd.mod -mod=readonly -o=$(GOBIN)/embedmd-v1.0.0 "github.com/campoy/embedmd"
 
 GOJSONTOYAML := $(GOBIN)/gojsontoyaml-v0.0.0-20200602132005-3697ded27e8c
 $(GOJSONTOYAML): .bingo/gojsontoyaml.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
 	@echo "(re)installing $(GOBIN)/gojsontoyaml-v0.0.0-20200602132005-3697ded27e8c"
-	@cd .bingo && $(GO) build -modfile=gojsontoyaml.mod -o=$(GOBIN)/gojsontoyaml-v0.0.0-20200602132005-3697ded27e8c "github.com/brancz/gojsontoyaml"
+	@cd .bingo && $(GO) build -modfile=gojsontoyaml.mod -mod=readonly -o=$(GOBIN)/gojsontoyaml-v0.0.0-20200602132005-3697ded27e8c "github.com/brancz/gojsontoyaml"
 
 GOLANGCI_LINT := $(GOBIN)/golangci-lint-v1.18.0
 $(GOLANGCI_LINT): .bingo/golangci-lint.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
 	@echo "(re)installing $(GOBIN)/golangci-lint-v1.18.0"
-	@cd .bingo && $(GO) build -modfile=golangci-lint.mod -o=$(GOBIN)/golangci-lint-v1.18.0 "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	@cd .bingo && $(GO) build -modfile=golangci-lint.mod -mod=readonly -o=$(GOBIN)/golangci-lint-v1.18.0 "github.com/golangci/golangci-lint/cmd/golangci-lint"
 
 JB := $(GOBIN)/jb-v0.2.0
 $(JB): .bingo/jb.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
 	@echo "(re)installing $(GOBIN)/jb-v0.2.0"
-	@cd .bingo && $(GO) build -modfile=jb.mod -o=$(GOBIN)/jb-v0.2.0 "github.com/jsonnet-bundler/jsonnet-bundler/cmd/jb"
+	@cd .bingo && $(GO) build -modfile=jb.mod -mod=readonly -o=$(GOBIN)/jb-v0.2.0 "github.com/jsonnet-bundler/jsonnet-bundler/cmd/jb"
 
 JSONNET := $(GOBIN)/jsonnet-v0.16.0
 $(JSONNET): .bingo/jsonnet.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
 	@echo "(re)installing $(GOBIN)/jsonnet-v0.16.0"
-	@cd .bingo && $(GO) build -modfile=jsonnet.mod -o=$(GOBIN)/jsonnet-v0.16.0 "github.com/google/go-jsonnet/cmd/jsonnet"
+	@cd .bingo && $(GO) build -modfile=jsonnet.mod -mod=readonly -o=$(GOBIN)/jsonnet-v0.16.0 "github.com/google/go-jsonnet/cmd/jsonnet"
 
 JSONNETFMT := $(GOBIN)/jsonnetfmt-v0.16.0
 $(JSONNETFMT): .bingo/jsonnetfmt.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
 	@echo "(re)installing $(GOBIN)/jsonnetfmt-v0.16.0"
-	@cd .bingo && $(GO) build -modfile=jsonnetfmt.mod -o=$(GOBIN)/jsonnetfmt-v0.16.0 "github.com/google/go-jsonnet/cmd/jsonnetfmt"
+	@cd .bingo && $(GO) build -modfile=jsonnetfmt.mod -mod=readonly -o=$(GOBIN)/jsonnetfmt-v0.16.0 "github.com/google/go-jsonnet/cmd/jsonnetfmt"
 
 UP := $(GOBIN)/up-v0.0.0-20200603110215-8a20b4e48ac0
 $(UP): .bingo/up.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
 	@echo "(re)installing $(GOBIN)/up-v0.0.0-20200603110215-8a20b4e48ac0"
-	@cd .bingo && $(GO) build -modfile=up.mod -o=$(GOBIN)/up-v0.0.0-20200603110215-8a20b4e48ac0 "github.com/observatorium/up/cmd/up"
+	@cd .bingo && $(GO) build -modfile=up.mod -mod=readonly -o=$(GOBIN)/up-v0.0.0-20200603110215-8a20b4e48ac0 "github.com/observatorium/up/cmd/up"
