745194b0844834f2e62c10c5574816c3f5b0467d
Simon Pasquier <spasquie@redhat.com>
2024-02-14 09:02:09 +0100

    MON-3708: fix test/integration.sh
    This change adjusts the scrape/rule intervals to reduce the test latency.
    It also adds logs to show how the test progresses.
    
    Signed-off-by: Simon Pasquier <spasquie@redhat.com>

--- test/integration.sh
+++ test/integration.sh
@@ -10,23 +10,23 @@ trap 'kill $(jobs -p); exit $result' EXIT
 
 ( ./authorization-server localhost:9001 ./test/tokens.json ) &
 
-( prometheus --config.file=./test/prom-local.conf --web.listen-address=localhost:9090 "--storage.tsdb.path=$(mktemp -d)" --log.level=warn ) &
+( prometheus --config.file=./test/prom-local.yaml --web.listen-address=localhost:9090 "--storage.tsdb.path=$(mktemp -d)" --log.level=warn ) &
 
-( 
+(
   sleep 5
   exec ./telemeter-client \
     --from "http://localhost:9090" \
     --to "http://localhost:9003" \
     --id "test" \
     --to-token a \
-    --interval 15s \
+    --interval 5s \
     --anonymize-labels "instance" --anonymize-salt "a-unique-value" \
     --rename ALERTS=alerts --rename openshift_build_info=build_info --rename scrape_samples_scraped=scraped \
     --match '{__name__="ALERTS",alertstate="firing"}' \
     --match '{__name__="scrape_samples_scraped"}'
 ) &
 
-( 
+(
 ./telemeter-server \
     --ratelimit=15s \
     --authorize http://localhost:9001 \
@@ -53,44 +53,47 @@ thanos query \
     --store=127.0.0.1:9006
 ) &
 
-sleep 1
-
-retries=100
+retries=1
 while true; do
-  if [[ "${retries}" -lt 0 ]]; then
-    echo "error: Did not successfully retrieve cluster metrics from the local Prometheus server" 1>&2
+  echo "info: attempt #${retries} $(date +%H:%M:%S)"
+  sleep 1
+
+  if [[ "${retries}" -gt 100 ]]; then
+    echo "error: Did not successfully retrieve cluster metrics from the local Prometheus server"
     exit 1
   fi
+
   # verify we scrape metrics from the test cluster and give it _id test
   if [[ "$( curl http://localhost:9008/api/v1/query --data-urlencode 'query=count({_id="test"})' -G 2>/dev/null | python3 -c 'import sys, json; print(json.load(sys.stdin)["data"]["result"][0]["value"][1])' 2>/dev/null )" -eq 0 ]]; then
-    retries=$((retries-1))
-    sleep 1
+    retries=$((retries+1))
     continue
   fi
+
   # verify we rename scrape_samples_scraped to scraped
   if [[ "$( curl http://localhost:9008/api/v1/query --data-urlencode 'query=count(scraped{_id="test"})' -G 2>/dev/null | python3 -c 'import sys, json; print(json.load(sys.stdin)["data"]["result"][0]["value"][1])' 2>/dev/null )" -eq 0 ]]; then
-    retries=$((retries-1))
-    sleep 1
+    retries=$((retries+1))
     continue
   fi
+
   # verify we got alerts as remapped from ALERTS
   if [[ "$( curl http://localhost:9008/api/v1/query --data-urlencode 'query=count(alerts{_id="test"})' -G 2>/dev/null | python3 -c 'import sys, json; print(json.load(sys.stdin)["data"]["result"][0]["value"][1])' 2>/dev/null )" -eq 0 ]]; then
-    retries=$((retries-1))
-    sleep 1
+    retries=$((retries+1))
     continue
   fi
+
   # verify we don't get elided labels
   if [[ "$( curl http://localhost:9008/api/v1/query --data-urlencode 'query=count(alerts{_id="test",_elide=~".+"})' -G 2>/dev/null | python3 -c 'import sys, json; print(len(json.load(sys.stdin)["data"]["result"]))' 2>/dev/null )" -gt 0 ]]; then
-    retries=$((retries-1))
-    sleep 1
+    retries=$((retries+1))
     continue
   fi
+
   break
 done
-echo "tests: ok"
+
+echo "info: tests ok"
 result=0
 exit 0
 
-echo "tests: failed" 1>&2
+echo "error: tests failed" 1>&2
 result=1
 exit 1
